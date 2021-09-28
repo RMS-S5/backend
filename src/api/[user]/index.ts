4bc827c2de948f6f6ca58ac5d3065d77d6df76b4
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2021-09-28 18:27:37 +0530

    .

--- src/api/[user]/index.ts
+++ src/api/[user]/index.ts
@@ -19,7 +19,7 @@ rUser.post("/register/customer", registerCustomer)
 
 // Update
 rUser.put('/update-user-profile', auth.any, updateUser.userProfile);
-rUser.put('/update-staff-member', auth.management, updateUser.staffMember);
-rUser.put('/update-password:userId', auth.any, updatePassword.updatePasswordByUserId);
+rUser.put('/update-staff-member/:userId', auth.management, updateUser.staffMember);
+rUser.put('/update-password/:userId', auth.any, updatePassword.updatePasswordByUserId);
 
 export default rUser
