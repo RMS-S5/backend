7f3d01895241aed56dd7125afcf6b9731555e590
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2021-10-01 21:57:52 +0530

    Fireabase msing implemented

--- src/api/[order]/update/order.ts
+++ src/api/[order]/update/order.ts
@@ -1,50 +1,111 @@
-import {EHandler, Handler} from "../../../utils/types";
-import model, {MErr} from "../../../model";
-import {v4 as UUID} from "uuid";
-import { body, inspectBuilder , param} from "../../../utils/inspect";
+import { EHandler, Handler } from "../../../utils/types";
+import model, { MErr } from "../../../model";
+import { v4 as UUID } from "uuid";
+import { body, inspectBuilder, param } from "../../../utils/inspect";
+import FireBaseService from "../../../utils/firebase/cloud_messaging";
 
 /**
  * Validate Request
  */
 
 const inspector = inspectBuilder(
-    param('orderId').exists().withMessage("Food item id is required"),
-    body("price")
-        .optional()
-        .isNumeric().withMessage("Total amount should be a number"),
-    body('orderStatus').optional(),
+  param("orderId").exists().withMessage("Food item id is required"),
+  body("price")
+    .optional()
+    .isNumeric()
+    .withMessage("Total amount should be a number"),
+  body("orderStatus").optional()
 );
 
 const updateOrder: Handler = async (req, res) => {
-    const { r } = res;
-    const orderId = req.params.orderId;
-    const { price, orderStatus } = req.body;
-    var tempData = { price, orderStatus };
-    var orderData;
-    if (req.user.accountType == model.user.accountTypes.waiter) {
-        orderData = {...tempData, waiterId : req.user.userId}
-    } else if (req.user.accountType == model.user.accountTypes.kitchenStaff) {
-        orderData = {...tempData, kitchenStaffId : req.user.userId}
-    }
-    
-    console.log(orderId);
-    const [error, response] =
-        await model.order.update_Order(orderId,
-            orderData);
-    if (error.code == MErr.NOT_FOUND) {
-        r.status.NOT_FOUND()
-            .message("Not found")
-            .send();
-        return;
-    }else if (error.code !== MErr.NO_ERROR) {
-        r.pb.ISE();
-        return;
-    }
+  const { r } = res;
+  const orderId = req.params.orderId;
+  const { price, orderStatus } = req.body;
+  var tempData = { price, orderStatus };
+  var orderData;
+  if (req.user.accountType == model.user.accountTypes.waiter) {
+    orderData = { ...tempData, waiterId: req.user.userId };
+  } else if (req.user.accountType == model.user.accountTypes.kitchenStaff) {
+    orderData = { ...tempData, kitchenStaffId: req.user.userId };
+  }
 
-    r.status.OK()
-        .message("Success")
-        .send();
+  console.log(orderId);
+
+  var message = {};
+  switch (orderStatus) {
+    case model.order.orderStatus.preparing:
+      message = {
+        notification: {
+          title: "Update about your order",
+          body: `Your order status is ${orderStatus}`,
+        },
+        data: {
+          orderStatus: orderStatus,
+        },
+        topic: "order-customer",
+      };
+      break;
+    case model.order.orderStatus.prepared:
+      message = {
+        notification: {
+          title: "New Orders",
+          body: `New prepared orders are added.`,
+        },
+        data: {
+          orderStatus: orderStatus,
+        },
+        topic: "order-waiter",
+      };
+      break;
+    case model.order.orderStatus.placed:
+      message = {
+        notification: {
+          title: "New Orders",
+          body: `New orders are placed.`,
+        },
+        data: {
+          orderStatus: orderStatus,
+        },
+        topic: "order-kitchen-staff",
+      };
+      break;
+    case model.order.orderStatus.waiterAssigned:
+      message = {
+        notification: {
+          title: "Order is assigned",
+          body: `Order is assigned to waiter`,
+        },
+        data: {
+          orderStatus: orderStatus,
+        },
+        condition:
+          "'order-kitchen-staff' in topics || 'order-waiter' in topics",
+      };
+      break;
+    default:
+      break;
+  }
+
+  try {
+    if (Object.keys(message).length != 0) {
+      const fireBaseAdmin = FireBaseService.getInstance();
+      await FireBaseService.sendMessageToTopics(fireBaseAdmin, message);
+    }
+  } catch (error) {
+    console.log(error);
+    return;
+  }
+
+  const [error, response] = await model.order.update_Order(orderId, orderData);
+  if (error.code == MErr.NOT_FOUND) {
+    r.status.NOT_FOUND().message("Not found").send();
+    return;
+  } else if (error.code !== MErr.NO_ERROR) {
+    r.pb.ISE();
+    return;
+  }
+
+  r.status.OK().message("Success").send();
 };
 
 export default [inspector, <EHandler>updateOrder];
-
