86acae8c95da3f4d9b4a1814d6555f369338c8dd
nuwanuom18 <nuwan.18@cse.mrt.ac.lk>
2021-10-23 11:21:36 +0530

    Adding changes to resolve issues after pr

--- src/api/[branch]/index.ts
+++ src/api/[branch]/index.ts
@@ -6,15 +6,16 @@ import addBranch from "./add/branch";
 import addTable from "./add/table";
 import removeBranch from "./remove/branch";
 import removeTable from "./remove/table";
-
+import getAvailabeRoomsHandler from "./get/allAvailableRooms";
+import getAllBrachesCustomer from "./get/allBrachesCustomer";
 
 const rBranch = Router();
 
 /**
  * Post
  */
- rBranch.post('/add-branch', auth.manager, addBranch);
- rBranch.post('/add-table', auth.branchManager, addTable);
+rBranch.post("/add-branch", auth.manager, addBranch);
+rBranch.post("/add-table", auth.branchManager, addTable);
 
 /**
  * Getters
@@ -25,16 +26,21 @@ rBranch.post(
 );
 rBranch.get("/get-all-branches-customer", getAllBrachesCustomer);
 
-rBranch.get("/table-by-verification/:verificationCode", getTableHandler.getTableByVerificationCode)
-rBranch.get("/branch-tables", auth.staffMember, getTableHandler.getBranchTables) //todo: include auth.branchManager
-rBranch.get("/branches", auth.management, getTableHandler.getBranches)
-
+rBranch.get(
+  "/table-by-verification/:verificationCode",
+  getTableHandler.getTableByVerificationCode
+);
+rBranch.get(
+  "/branch-tables",
+  auth.staffMember,
+  getTableHandler.getBranchTables
+); //todo: include auth.branchManager
+rBranch.get("/branches", auth.management, getTableHandler.getBranches);
 
 /**
-* Delete
-*/
-rBranch.put('/remove-branch/:branchId', auth.manager, removeBranch);
-rBranch.put('/remove-table/:tableNumber', auth.branchManager, removeTable);
-
+ * Delete
+ */
+rBranch.put("/remove-branch/:branchId", auth.manager, removeBranch);
+rBranch.put("/remove-table/:tableNumber", auth.branchManager, removeTable);
 
 export default rBranch;
