e53f4eab9aa754249d0f37dea2f8f78732fb7160
Pasindu <pasindua.18@cse.mrt.ac.lk>
2021-11-10 06:47:01 +0530

    comment1

--- src/api/[order]/get/order.ts
+++ src/api/[order]/get/order.ts
@@ -4,13 +4,21 @@ import {MErrorCode} from "../../../utils/dbMan/merror";
 import {inspectBuilder,query} from "../../../utils/inspect";
 import table from "../../[branch]/add/table";
 
-
+/**
+ * Inspector
+ */
 const tableOrderInspector = inspectBuilder(
     query('tableNumber').exists().withMessage("Table Number is required"),
     query('branchId').exists().withMessage("Branch id is required"),
 
 )
 
+/**
+ * 
+ * @param req Get all active orders
+ * @param res 
+ * @returns 
+ */
 const getActiveOrders: Handler = async (req, res) => {
     const {r} = res;
 
@@ -26,23 +34,6 @@ const getActiveOrders: Handler = async (req, res) => {
         .message("Success")
         .send();
 };
-
-// const getAllServedOrders: Handler = async (req, res) => {
-//     const {r} = res;
-
-//     const [error, ordersData] = await
-//         model.order.get_AllOrders({orderStatus : model.order.orderStatus.served,
-//         branchId : req.user?.branchId});
-//     if (error.code !== MErr.NO_ERROR) {
-//         r.pb.ISE();
-//         return;
-//     }
-
-//     r.status.OK()
-//         .data(ordersData)
-//         .message("Success")
-//         .send();
-// };
 
 /**
  * Table order
